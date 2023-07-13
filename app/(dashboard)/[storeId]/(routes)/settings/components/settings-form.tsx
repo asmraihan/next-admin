481ecb28ed0f951d6352eaf6f0ae0eb4782ec6be
asmraihan <asmraihanctg14@gmail.com>
2023-07-13 13:17:58 +0600

    store DELETE api

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import AlertModal from '@/components/modals/alert-modal'
 import { Button } from '@/components/ui/button'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import Heading from '@/components/ui/heading'
@@ -52,8 +53,29 @@ const SettingsForm: React.FC<SettingsFormProps> = ({
         }
     }
 
+    const onDelete = async() => {
+        try {
+            setLoading(true)
+            await axios.delete(`/api/stores/${params.storeId}`)
+            router.refresh()
+            router.push('/')
+            toast.success("Store deleted")
+        } catch (error) {
+            toast.error("Make sure you removed all products and categories first")
+        } finally {
+            setLoading(false)
+            setOpen(false)
+        }
+    }
+
     return (
         <>
+        <AlertModal 
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+        />
             <div className='flex items-center justify-between'>
                 <Heading
                     title='Settings'
