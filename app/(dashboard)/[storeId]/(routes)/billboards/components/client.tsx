20482907870a11010bb38439661ffc13303bd943
asmraihan <asmraihanctg14@gmail.com>
2023-07-25 01:13:53 +0600

    billboard filter

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -31,7 +31,7 @@ const BillboardClient: React.FC<BillboardClientProps> = ({
                 </Button>
             </div>
             <Separator />
-            <DataTable columns={columns} data={data} />
+            <DataTable searchKey='label' columns={columns} data={data} />
         </>
     )
 }
