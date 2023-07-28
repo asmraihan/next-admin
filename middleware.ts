edb04f6a1d052e6565f5a002362dde24d6bc30bc
asmraihan <asmraihanctg14@gmail.com>
2023-07-28 16:20:48 +0600

    apis public

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/api/:path*"] // to open get api routes to public
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
