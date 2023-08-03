3d0b2700b87b180d0f1524bfcdaeca852e1692ff
asmraihan <asmraihanctg14@gmail.com>
2023-08-04 01:39:24 +0600

    cart page check

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -69,9 +69,9 @@ export async function POST(
         phone_number_collection: {
             enabled: true
         },
-        // success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
-        success_url: `${process.env.FRONTEND_STORE_URL}?success=1`,
-        cancel_url: `${process.env.FRONTEND_STORE_URL}?canceled=1`,
+        // success_url: `${process.env.FRONTEND_STORE_URL}?success=1`,
+        success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
+        cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
         metadata: {
             orderId: order.id
         }     
