6d51bf18a57a3936f5327b203732dfdfeff84fe2
asmraihan <asmraihanctg14@gmail.com>
2023-08-04 00:11:47 +0600

    dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ModalProvider } from '@/providers/modal-provider'
 import prismadb from '@/lib/prismadb'
 import { ToasterProvider } from '@/providers/toast-provider'
+import { ThemeProvider } from '@/providers/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,9 +24,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ToasterProvider></ToasterProvider>
-          <ModalProvider />
-          {children}
+          <ThemeProvider attribute='class' defaultTheme='light' enableSystem>
+            <ToasterProvider></ToasterProvider>
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import { MainNav } from '@/components/main-nav'
 import StoreSwitcher from '@/components/store-switcher'
 import { redirect } from 'next/navigation'
 import prismadb from '@/lib/prismadb'
+import { ThemeToggle } from '@/components/theme-toggle'
 
 const Navbar = async() => {
     const {userId} = auth()
@@ -22,6 +23,7 @@ const Navbar = async() => {
                 <StoreSwitcher items={stores}/>
                 <MainNav className='mx-6' />
                 <div className='ml-auto flex items-center space-x-4'>
+                    <ThemeToggle />
                     <UserButton afterSignOutUrl='/'></UserButton>
                 </div>
             </div>
